42966fd17b6b1a293f3d9a3be1ebb45a8b52eb3a
franjpr <franpr90@gmail.com>
2017-03-10 18:15:45 +0000

    tests

--- src/app.js
+++ src/app.js
@@ -1,33 +1,50 @@
 var express = require('express');
 var cookieparser = require('cookie-parser');
+var bodyparser = require('body-parser');
 var session = require('express-session');
 var app = express();
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 9090);
 app.set('views', __dirname + '/views');
 app.set('viewengine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.use(cookieparser());
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
 
 app.use(session({
-  secret: 'my-secret',
+  secret: 'my-secret-key',
   saveUninitialized: false,
   resave: false
 }));
 
-app.use('/', function (req, res, next){
+app.use('/', function (req, res, next) {
   console.log(req.cookies);
-  console.log('***********');
+  console.log('------------');
   console.log(req.session);
   next();
-})
-
-app.get('/', function (req, res, next) {
-
-  res.render('index.ejs', {});
-
 });
+
+app.get('/', function(req, res){
+  var html = `
+        <form action="/" method="post">
+           Your name: <input type="text" name="userName"><br>
+           <button type="submit">Submit</button>
+        </form>
+  `;
+  if (req.session.userName) {
+    html += '<br>Your username from your session is: ' + req.session.userName;
+  }
+  res.send(html);
+});
+
+app.post('/', function(req, res){
+  req.session.userName = req.body.userName;
+  res.redirect('/');
+});
+
 
 
 
