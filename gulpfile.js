6277b9b53eb20db333ad23d786e889426b9f6ceb
franjpr <franpr90@gmail.com>
2017-03-11 22:06:28 +0000

    nada

--- gulpfile.js
+++ gulpfile.js
@@ -16,12 +16,12 @@ gulp.task('update-book', function(){
   exec('git ls-remote --heads https://github.com/ULL-ESIT-PL-1617/estudiar-cookies-y-sessions-en-expressjs-ale-fran.git book', function(err, out, errout){
     if(err) console.log('Error checking if branch exists \n' + err);
     else {
-      if(out.length ==  0){ //No existe, entonces creamos la rama
+      if(out.length ==  0 && out.includes('fatal')){ //No existe, entonces creamos la rama
         exec('git checkout -b book', function (err, out, errout){
           if(err) console.log('Error, couldnt create branch \n' + err);
           else {
             console.log('Branch "book" created'); //Filtramos el contenido de ./docs a la rama
-            exec('git filter-branch --subdirectory-filter ./docs book', function(err, our, errout){
+            exec('git filter-branch --subdirectory-filter -f ./docs book', function(err, our, errout){
               if(err) console.error('Error filtering branch content \n' + err);
               else {
                 exec('git checkout book && git add . && git commit -m "update-gitbook" && git push -f gbook book:master', function (err, out, errout) {
@@ -33,7 +33,7 @@ gulp.task('update-book', function(){
           }
         });
      } else {
-       exec('git filter-branch --subdirectory-filter ./docs book', function(err, our, errout){
+       exec('git filter-branch --subdirectory-filter -f ./docs book', function(err, our, errout){
          if(err) console.error('Error filtering branch content \n' + err);
          else {
            console.log('Filtered ./docs to "book" branch');
