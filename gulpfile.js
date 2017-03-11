d7009dbd10e753aef3c7e3eb27b317bb0b4e76fd
franjpr <franpr90@gmail.com>
2017-03-11 19:45:02 +0000

    testeando gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var gh-page = require('gulp-gh-pages');
+var ghpage = require('gulp-gh-pages');
 var exec = require('child_process').exec;
 var path = require('path').resolve() + '\\node_modules\\.bin\\';
 
@@ -7,21 +7,41 @@ gulp.task('create-book', function(){
   var cmd = 'gitbook build docs htmls';
   exec(path + cmd, function(err, out, errout){
     if(err) console.error('Error:' + err);
-    else{
-      console.log('Build success');
-      var newbranch = 'git filter-branch --subdirectory-filter ./docs gitbook';
-      exec(newbranch, function(err, our, errout){
-        if(err) console.error('Error:' + err);
-        else{
-          console.log('Filtered branch "gitbook" created succesfully');
-        }
-      });
-    }
+    else console.log('Gitbook build success');
   });
 });
 
+
+
 gulp.task('update-book', function(){
-  var remote = 'gbook';
+ var branch_exists =  'git ls-remote --heads https://github.com/ULL-ESIT-PL-1617/estudiar-cookies-y-sessions-en-expressjs-ale-fran.git book'
+ exec(branch_exists, function(err, out, errout){
+   if(err) console.log('Error: ' + err);
+   else {
+     if(out.length ==  0){ //No existe, entonces creamos la rama
+       exec('git checkout -b book', function (err, out, errout){
+         if(err) console.log('Error, couldnt create branch');
+         else {
+           console.log('Branch "book" created');
+
+           var newbranch = 'git filter-branch --subdirectory-filter ./docs gitbook';
+           exec(newbranch, function(err, our, errout){
+             if(err) console.error('Error:' + err);
+             else{
+               console.log('Filtered branch "gitbook" created succesfully');
+             }
+           });
+         }
+       });
+     } else{
+
+
+     }
+
+   }
+ });
+
+var remote = 'gbook';
   var cmd = 'git checkout gitbook && git add . && git commit -m "update-docs" && git push -f gbook gitbook:master'
   exec(cmd, function (err, out, errout) {
     if(err) console.log("Error updating gitbook branch \n" + err);
