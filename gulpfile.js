44071c9a91224a4aa37225ba00aea171ba58945c
franjpr <franpr90@gmail.com>
2017-03-12 19:01:27 +0000

    Deploy gitbook

--- gulpfile.js
+++ gulpfile.js
@@ -19,11 +19,12 @@ gulp.task('deploy-gh-pages', function(){
 gulp.task('deploy-gitbook', function () {
   console.log('Deploying gitbook');
     exec('git add . && git commit -m "Deploy gitbook" && git push origin master', function (err, out, errout) {
-      console.log('Pushing local changes... \n' + errout);
+      console.log('Pushing local changes...');
       exec('git push origin --delete book && git branch -D book', function (err, out, errout){
-        console.log('Cleaning book branch... \n' + errout);
-        console.log('Creating book branch...');
-          exec('git checkout -b book && git filter-branch --subdirectory-filter ./docs -f book && echo "!*.md" >> .gitignore', function (err, out, errout){
+        console.log('Cleaning book branch...');
+        console.log('Creating a new book branch...');
+        var gitignore = 'echo "node_modules/ >> .gitignore && echo "htmls/ >> .gitignore && echo ".publish/ >> .gitignore'
+          exec('git checkout -b book && git filter-branch --subdirectory-filter ./docs -f book && ' + gitignore, function (err, out, errout){
             console.log('Filtering book content from ./docs \n' + errout);
             exec('git add . && git commit -m "update bookbranch" && git push origin book && git push -f gbook book:master && git checkout master', function (err, out, errout){
               console.log('Pushing local changes to github & gitbook \n' + errout);
